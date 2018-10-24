3ebf715ce55630e3de6931e3d5976d7261a6142f
Markus Ott <maott@tsn.at>
2018-10-24 14:15:23 +0200

    Test Klasse hinzugefügt

---
+++ src/sample/Test.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("test");
+
+
+
+    }
+
+}
